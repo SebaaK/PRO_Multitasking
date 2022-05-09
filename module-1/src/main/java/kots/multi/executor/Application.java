504f1a1c0301a5479a161c33dc1f47cea587f864
SebaaK <sebaa.kot@gmail.com>
2022-05-09 12:39:01 +0200

    Not important learning etaps

---
+++ module-1/src/main/java/kots/multi/executor/Application.java
@@ -0,0 +1,16 @@
+package kots.multi.executor;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+class Application {
+
+    public static void main(String[] args) {
+        int cores = Runtime.getRuntime().availableProcessors();
+        ExecutorService executor = Executors.newFixedThreadPool(cores);
+        for(int n = 0; n < 1000; n++) {
+            executor.submit(new Task());
+        }
+        executor.shutdown();
+    }
+}
