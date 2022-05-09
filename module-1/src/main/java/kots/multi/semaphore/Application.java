377759509fdc027dc98ea2ad1c0c173d36664b64
SebaaK <sebaa.kot@gmail.com>
2022-05-09 13:18:49 +0200

    Not important learning etaps

---
+++ module-1/src/main/java/kots/multi/semaphore/Application.java
@@ -0,0 +1,15 @@
+package kots.multi.semaphore;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+class Application {
+
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newCachedThreadPool();
+        for(int n = 0; n < 1000; n++) {
+            executor.submit(new DbManager());
+        }
+        executor.shutdown();
+    }
+}
